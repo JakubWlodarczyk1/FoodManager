3517685c3eaa1b5e6baa068a06f0b14005d0a72c
Jakub Włodarczyk <kubawlo1@gmail.com>
2024-11-07 17:03:44 +0100

    Add TranslationKey to Category

--- FoodManager.Domain/Entities/Category.cs
+++ FoodManager.Domain/Entities/Category.cs
@@ -6,10 +6,19 @@ namespace FoodManager.Domain.Entities
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public CategoryType Type { get; set; }
 
+        private string _name = default!;
+        public required string Name
+        {
+            get => _name;
+            set => _name = value.ToLowerInvariant();
+        }
+
+        public CategoryType Type { get; set; }
+        public string? TranslationKey { get; set; }
         public string? CreatedById { get; set; }
         public IdentityUser? CreatedBy { get; set; }
+
+        public void EncodeName() => Name = Name.ToLower();
     }
 }
